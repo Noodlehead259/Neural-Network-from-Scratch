b67c7d0c74683a78817d11490637760439765c8e
Noodlehead259 <vermaarman259@gmail.com>
2025-05-29 18:12:38 +0530

    activation functions and loss function made

--- Neural Network.py
+++ Neural Network.py
@@ -6,18 +6,33 @@ import numpy as np
 
 #creating neuron class
 class neuron:
-    def __init__(self, weights, bias, inputs):
+    def __init__(self, weights, bias):
         self.weights = weights
         self.bias = bias
-        self.inputs = inputs
 
-    def output(self):
-        size = len(self.weights)
-        ans = 0
-        for i in range(size):
-            ans += self.weights[i] * self.inputs[i]
-        return ans + self.bias
+    def output(self, inputs):
+        return np.dot(self.weights, inputs) + self.bias
     
+
+#activation functions
+def relu(x):
+    return np.maximum(0, x)
+
+def relu_derivative(x):
+    return (x > 0).astype(float)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    s = sigmoid(x)
+    return s * (1 - s)
+
+#loss function
+def binary_cross_entropy(y_true, y_pred):
+    eps = 1e-10
+    return -np.mean(y_true * np.log(y_pred + eps) + (1 - y_true) * np.log(1 - y_pred + eps))
+
 
 
 #importing and applying one-hot encoding to the dataset
